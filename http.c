e8e2ef4254408b7f7cf10c33079a374810f8f708
rahma <rahmatanko@outlook.com>
2026-01-01 23:33:42 +0300

    docs: fixed http.c comments

--- http.c
+++ http.c
@@ -6,19 +6,18 @@
 // we need the request itself and also a buffer to write the path into
 int parse_request(char * request, char * path) {
     // variables to hold the method (GET, POST) and version (1.0, 1.1, etc)
-    // size is one value larger than the string itself is to make space for the null character
+    // size is one value larger than the string itself to make space for the null character
     char method[8], version[16];
 
     // a correct request would in the form 'GET /file HTTP/1.0'
-    //sscanf returns the no. of successfully filled variables, so anything other than 3 is not successful
+    // sscanf returns the no. of successfully filled variables, so anything other than 3 is not successful
     if (sscanf(request, "%7s %255s %15s", method, path, version) != 3) return -1;
 
     // strcmp returns the difference between characters in strings, so if it isn't 0, it means the strings are not the same
+    // we have to make sure it's a GET request of version HTTP/1.0
     if (strcmp(method, "GET")) return -1;
-
-    // strcmp returns the difference between characters in strings, so if it isn't 0, it means the strings are not the same
     if (strcmp(version, "HTTP/1.0")) return -1;
 
-    // if we reached ehre, this was a success
+    // if we reached here, this was a success
     return 0;
 }

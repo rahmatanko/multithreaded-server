8169585c407035a16157aa033880d99fe4e72161
rahma <rahmatanko@outlook.com>
2026-01-02 22:21:39 +0300

    fix: added relevant data to http headers

--- file.c
+++ file.c
@@ -5,35 +5,54 @@
 void file_contents(int accepted, char * path) {
 
     // the struct will give filesystem metadata about a path
-    struct stat info; FILE *f;
+    struct stat info;
+    FILE *f;
 
-    // if stat returns 0  it means it now has file info
-    // we open the file in binary mode instead of the regular way to be mindful of all filetypes, curroption is a risk if the file isnt txt otherwise
-    // if the file is empty, then our response is 404, or if the file is actually a directory then its an error as well
-    // otherwise if there's another problem in opening the file then the error is the same
+    // if stat returns < 0 it means it failed
+    // if the file is empty, or if the path is a directory, or if opening fails -> 404
     if (stat(path, &info) < 0 || S_ISDIR(info.st_mode) || !(f = fopen(path, "rb"))) {
+        const char *body = "404 Not Found\n";
+
         perror("file open failed");
-        write(accepted, "HTTP/1.0 404 Not Found\r\n\r\n", 26);
+
+        dprintf(accepted,
+                "HTTP/1.0 404 Not Found\r\n"
+                "Content-Length: %zu\r\n"
+                "Connection: close\r\n"
+                "\r\n"
+                "%s",
+                strlen(body), body);
+
+        close(accepted);
+        return;
     }
 
-    else {
-        // otherwise the file was opened successfully
-        write(accepted, "HTTP/1.0 200 OK\r\n\r\n", 19);
+    // otherwise the file was opened successfully
+    size_t file_size = info.st_size;
 
-        // now the file is streamed to the client socket
-        char data[SIZE];
-        // used size_t because it always matches memory size which is convenient
-        size_t n;
+    dprintf(accepted,
+            "HTTP/1.0 200 OK\r\n"
+            "Content-Length: %zu\r\n"
+            "Connection: close\r\n"
+            "\r\n",
+            file_size);
 
-        // we read/write a chunk of data once, for as long as there is data to read/write
-        while ((n = fread(data, 1, sizeof(data), f)) > 0) {
-            write(accepted, data, n);
-        }
+    // now the file is streamed to the client socket
+    char data[SIZE];
+    size_t n;
 
-        // close the file or else memory leaks happen
-        fclose(f);
+    // read/write file contents
+    while ((n = fread(data, 1, sizeof(data), f)) > 0) {
+        write(accepted, data, n);
     }
+
+    // close the file or else memory leaks happen
+    fclose(f);
+
+    // close client socket so client knows response is finished
+    close(accepted);
 }
+
 
 // gets the size of the file
 int get_file_size(char *path, size_t *file_size) {

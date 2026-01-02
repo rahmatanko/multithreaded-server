e42752a73ab48ace18830ec8906ae29f254df35b
rahma <rahmatanko@outlook.com>
2026-01-02 20:36:31 +0300

    feat: updated worker func to use the worker struct and log the worker

--- worker.c
+++ worker.c
@@ -1,27 +1,19 @@
 #include "worker.h"
 
 void * worker(void * arg) {
-    requeue_t * q = (requeue_t *) arg;
+    worker_t * worker = (requeue_t *) arg;
 
     req_t req;
 
     while (1){
         // get the request from the queue
-        req = dequeue(q);
+        req = dequeue(worker->queue);
 
-        // get the header
-        if (get_header(req.client, req.header)) {close(req.client); continue;}
+        // log the worker who picker the request
+        log_worker(worker->id, &req);
 
-        // attempt to get the path from the header
-        if (!get_path(req.header, req.path)) {
-            // get the file contents, if any
-            file_contents(req.client, req.path + 1); // skipped the leading '/'
-        }
-
-        else {
-            // otherwise the request is invalid
-            write(req.client, "HTTP/1.0 400 Bad Request\r\n\r\n", 28);
-        }
+        // get the file contents
+        file_contents(req.client, req.path + 1); // skipped the leading '/'
 
         // closing the socket to avoid file descriptor leak
         close(req.client);
--- worker.h
+++ worker.h
@@ -5,6 +5,13 @@
 #include "queue.h"
 #include "http.h"
 
+// represents a single worker thread
+typedef struct worker {
+    int id;              // unique worker ID (used in logs)
+    pthread_t thread;    // the actual pthread
+    requeue_t *queue;    // pointer to the shared request queue
+} worker_t;
+
 void * worker(void * arg);
 
 #endif 
